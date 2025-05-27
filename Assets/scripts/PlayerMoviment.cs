dea14cacc61211eb134268d65f595c607014461c
LuizFelipeMartins01 <luizmartins151208@gmail.com>
2025-05-27 17:37:27 -0300

    quarto commit

--- Assets/scripts/PlayerMoviment.cs
+++ Assets/scripts/PlayerMoviment.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 
-public class PlayerMoviment : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private float inputH;
     private float inputV;
     private Animator animator;
-    [SerializeField] private float Velocidade;
+    private bool estaNoChao = true;
+    private Vector3 anguloRotacao = new Vector3(0, 90, 0);
+    [SerializeField] private float velocidade;
     [SerializeField] private float forcaPulo;
-    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,49 +22,90 @@ public class PlayerMoviment : MonoBehaviour
     void Update()
     {
         Andar();
+        Girar();
+        Pular();
     }
-    private void Andar() 
+
+    private void Andar()
     {
         inputV = Input.GetAxis("Vertical");
-        transform.position += new Vector3(0, 0, inputH * Velocidade * Time.deltaTime);
-        if(Input.GetKey(KeyCode.W))
+        Vector3 moveDirection = transform.forward * inputV;
+        Vector3 moveForward = rb.position + moveDirection * velocidade * Time.deltaTime;
+        rb.MovePosition(moveForward);
+
+        if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("Andar", true);
+            animator.SetBool("AndarParaTras", false);
         }
-        else if(Input.GetKeyDown(KeyCode.W))
+        else if(Input.GetKey(KeyCode.S))
         {
-            animator.SetBool("Andar" , false);
+            animator.SetBool("AndarParaTras", true);
+            animator.SetBool("Andar", false);
         }
+        //else
+        //{
+        //    animator.SetBool("Andar", false);
+        //    animator.SetBool("Andar", false);
+        //}
     }
+
     private void Girar()
     {
+        inputH = Input.GetAxis("Horizontal");
+        Quaternion deltaRotation =
+            Quaternion.Euler(anguloRotacao * inputH * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
 
+        //if (Input.GetKey(KeyCode.A) ||
+        //            Input.GetKey(KeyCode.D) ||
+        //                Input.GetKey(KeyCode.LeftArrow) ||
+        //                    Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    animator.SetBool("Andar", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("Andar", false);
+        //}
     }
+
     private void Pular()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            estaNoChao = false;
+            animator.SetTrigger("Pular");
+        }
     }
+
     private void Correr()
     {
 
     }
+
     private void Morrer()
     {
 
     }
-    private void interagir()
+
+    private void Interagir()
     {
 
     }
+
     private void Pegar()
     {
 
     }
+
     private void Atacar()
     {
 
     }
-    private void AtacarDois()
+
+    private void Magia()
     {
 
     }
